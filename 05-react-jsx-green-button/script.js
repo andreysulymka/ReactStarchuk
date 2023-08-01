b410536373292fa7a2030feb8fee9e9ffdb58f10
andreysulymka <andreysulymka5@gmail.com>
2023-08-02 00:29:37 +0300

    add create-react-app

--- 05-react-jsx-green-button/script.js
+++ 05-react-jsx-green-button/script.js
@@ -1,17 +1,20 @@
-const App = () => {
-  let [buttonText, setButtonText] = React.useState("Click Me");
-  console.log(buttonText);
+const App = ({ initialButtonText, initialButtonClass }) => {
+  const [buttonText, setButtonText] = React.useState(initialButtonText);
+  const [clasesList, setClasesList] = React.useState(initialButtonClass);
 
   const onButtonClick = () => {
     setButtonText("Hello from React");
+    setClasesList("green-btn");
   };
 
   return (
     <div className="app">
-      <button onClick={onButtonClick}>{buttonText}</button>
+      <button className={clasesList} onClick={onButtonClick}>
+        {buttonText}
+      </button>
     </div>
   );
 };
 const container = document.getElementById("app");
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(<App initialButtonText="Click me" initialButtonClass="" />);
