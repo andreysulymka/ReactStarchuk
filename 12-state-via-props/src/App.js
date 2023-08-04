c05924505d5554c8660d7ebbf8e344092f2ac863
andreysulymka <andreysulymka5@gmail.com>
2023-08-04 18:12:14 +0300

    add 12 project

---
+++ 12-state-via-props/src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+import "./App.css";
+import Button from "./components/Button";
+import Counter from "./components/Counter";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <Counter count={count} />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
