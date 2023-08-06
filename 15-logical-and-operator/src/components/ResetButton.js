842d0b07c188094ba16d1a9d5a83d182e3c00182
andreysulymka <andreysulymka5@gmail.com>
2023-08-06 16:51:50 +0300

    add project with logical render

---
+++ 15-logical-and-operator/src/components/ResetButton.js
@@ -0,0 +1,11 @@
+function ResetButton({ onClick }) {
+  return (
+    <div>
+      <button style={{ backgroundColor: "lightgreen" }} onClick={onClick}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default ResetButton;
