f52209cc72ba8e88a98e80951c35a0a35d32ad5f
andreysulymka <andreysulymka5@gmail.com>
2023-08-11 18:03:29 +0300

    add react router navigation

--- 26-react-router/src/App.jsx
+++ 26-react-router/src/App.jsx
@@ -4,16 +4,20 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Contacts from "./components/Contacts";
 import NotFound from "./components/NotFound";
+import MainLayout from "./Layouts/MainLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/contacts" element={<Contacts/>} />
+          <Route path="/" element={<MainLayout/>}>
+          <Route index element={<Home/>}/>
+          <Route path="about" element={<About/>} />
+          <Route path="contacts" element={<Contacts/>} />
           <Route path="*" element={<NotFound/>} />
+          </Route>
+          
         </Routes>
       </div>
       ;
