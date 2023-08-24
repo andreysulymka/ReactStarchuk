3f735785ba7eb6c640f6aea3af6c8b069d6dfb7b
andreysulymka <andreysulymka5@gmail.com>
2023-08-24 18:56:28 +0300

    add styles

--- 26-react-router/src/components/Menu.jsx
+++ 26-react-router/src/components/Menu.jsx
@@ -1,14 +1,35 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 const Menu = () => {
   return (
     <nav>
-     <NavLink to='.' end>Home</NavLink>
-     <NavLink to='/courses'>Courses</NavLink>
-     <NavLink to='/about'>About</NavLink>
-     <NavLink to='/contacts'>Contacts</NavLink>
-     </nav>
-  )
-}
+      <NavLink
+        style={({isActive}) => isActive ? {color: 'lightyellow', textDecoration: 'none'}: {}}
+        to="."
+        end
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "activeLink" : "link")}
+        to="/courses"
+      >
+        Courses
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "activeLink" : "link")}
+        to="/about"
+      >
+        About
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "activeLink" : "link")}
+        to="/contacts"
+      >
+        Contacts
+      </NavLink>
+    </nav>
+  );
+};
 
-export default Menu
+export default Menu;
