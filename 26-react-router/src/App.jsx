c050f0f53d0ec4c6841e29004ff07b05f9bec10b
andreysulymka <andreysulymka5@gmail.com>
2023-08-25 23:57:17 +0300

    add some new

--- 26-react-router/src/App.jsx
+++ 26-react-router/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contacts from "./components/Contacts";
@@ -7,21 +6,21 @@ import NotFound from "./components/NotFound";
 import MainLayout from "./Layouts/MainLayout";
 import Courses from "./components/Courses";
 import SingleCourse from "./components/SingleCourse";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<MainLayout/>}>
-          <Route index element={<Home/>}/>
-          <Route path="about" element={<About/>} />
-          <Route path="contacts" element={<Contacts/>} />
-          <Route path="courses" element={<Courses/>}/>
-          <Route path="courses/:slug" element={<SingleCourse/>}/>
-          <Route path="*" element={<NotFound/>} />
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contacts" element={<Contacts />} />
+            <Route path="courses" element={<Courses />} />
+            <Route path="courses/:slug" element={<SingleCourse />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
-          
         </Routes>
       </div>
       ;
--- 26-react-router/src/components/SingleCourse.jsx
+++ 26-react-router/src/components/SingleCourse.jsx
@@ -1,10 +1,19 @@
-import {useParams} from 'react-router-dom'
-import courses from '../data/courses'
+import { Link, useParams } from "react-router-dom";
+import courses from "../data/courses";
 
 const SingleCourse = () => {
-    const params = useParams()
-    console.log(params)
-  return <h1>Single Course Info</h1>;
+  const params = useParams();
+  const course = courses.find((course) => course.slug === params.slug);
+  return (
+    <>
+      <h1>{course.title}</h1>
+      <h2>{course.slug}</h2>
+      <h3>{course.id}</h3>
+      <Link to=".." relative="path">
+        All courses
+      </Link>
+    </>
+  );
 };
 
 export default SingleCourse;
