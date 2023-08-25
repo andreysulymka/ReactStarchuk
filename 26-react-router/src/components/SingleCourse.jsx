151b09129c3e8733118573ee6013d8fea262b9b6
andreysulymka <andreysulymka5@gmail.com>
2023-08-26 00:27:57 +0300

    add redirecting with useNavigate

--- 26-react-router/src/components/SingleCourse.jsx
+++ 26-react-router/src/components/SingleCourse.jsx
@@ -1,14 +1,29 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import courses from "../data/courses";
+import NotFound from "./NotFound";
+import { useEffect } from "react";
 
 const SingleCourse = () => {
   const params = useParams();
+  const navigate = useNavigate();
   const course = courses.find((course) => course.slug === params.slug);
+
+  // Simply show NotFound Component
+  // if (!course) {
+  //   return <NotFound />;
+  // }
+
+  useEffect(() => {
+    if (!course) {
+      navigate("..", { relative: "path" });
+    }
+  }, [course]);
+
   return (
     <>
-      <h1>{course.title}</h1>
-      <h2>{course.slug}</h2>
-      <h3>{course.id}</h3>
+      <h1>{course?.title}</h1>
+      <h2>{course?.slug}</h2>
+      <h3>{course?.id}</h3>
       <Link to=".." relative="path">
         All courses
       </Link>
